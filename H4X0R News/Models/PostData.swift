214b6a213dbb31ea8ebe67cbcbbc8c976aad74f7
David <davidtucu@hotmail.com>
2021-06-10 19:47:59 -0500

    Run on Mac

---
+++ H4X0R News/Models/PostData.swift
@@ -0,0 +1,21 @@
+//
+//  PostaData.swift
+//  H4X0R News
+//
+//  Created by david on 10/06/2021.
+//
+
+import Foundation
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
---
+++ H4X0R News/Views/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by david on 10/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var networkManager = NetworkManager()
+    var body: some View {
+        NavigationView{
+            List(networkManager.posts){
+                post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+                
+            }
+            .navigationBarTitle("h4X0R NEWS")
+        }
+        .onAppear{
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
